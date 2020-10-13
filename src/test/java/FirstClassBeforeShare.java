9e779aced018ddd1b57e20b13b0e3e5600f96e2c
gulgulyil <gulnurgulnurxxf@gmail.com>
2020-10-13 21:32:42 +0200

    commit1

--- src/test/java/FirstClassBeforeShare.java
+++ src/test/java/FirstClassBeforeShare.java
@@ -2,5 +2,6 @@ public class FirstClassBeforeShare {
     public static void main(String[] args) {
         System.out.println("Hi everyone!!");
         //this file was created before sharing to all of you. First class.
+        System.out.println("Hi everyone!!");
     }
 }
